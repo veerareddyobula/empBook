f4cbdea35aceac6c12d9580a4c0217f2cb73d3a9
Veera Reddy Obula <veerareddy.obula@gmail.com>
2019-04-14 11:56:29 -0700

    Created Ionic-V1 app with Angular1.x version

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'firebase', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -35,8 +35,19 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login-view.html',
+    controller: 'LoginCtrl',
+    controllerAs: 'vm',
+  })
+  .state('register', {
+    url: '/register',
+    templateUrl: 'templates/register-view.html',
+    controller: 'RegisterCtrl'
+  })
   // setup an abstract state for the tabs directive
-    .state('tab', {
+  .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
@@ -84,6 +95,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/login');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,14 +3,6 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
@@ -25,4 +17,50 @@ angular.module('starter.controllers', [])
   $scope.settings = {
     enableFriends: true
   };
+})
+.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state, $firebaseAuth){
+  var auth = $firebaseAuth();
+
+  $scope.data = {};
+  $scope.login = function() {
+      console.log("LOGIN user: " + $scope.data.username + " - PW: " + $scope.data.password);
+      auth.$signInWithEmailAndPassword($scope.data.username, $scope.data.password).then(function(snapshot){
+        console.log('--- addNewUser snapshot --==> ', snapshot);
+        $state.go('tab.dash');
+      }, function(error){
+        console.log('--- addNewUser error    --==> ', error);
+        $ionicPopup.alert({
+          title: 'Login failed!',
+          template: error.message
+        });
+      })
+  }
+
+  $scope.register = function() {
+    $state.go('register');
+  }
+})
+.controller('RegisterCtrl', function($scope, LoginService, $ionicPopup, $state, $firebaseAuth){
+  var auth = $firebaseAuth();
+  console.log('--=== This is RegisterCtrl ===---')
+  $scope.data = {};
+  $scope.isInValied = function() {
+    if($scope.data.password && ($scope.data.password === $scope.data.confirmPassword)){
+      return false;
+    }
+    return true;
+  }
+  $scope.registerNewUser = function() {
+      console.log("register New-User : ", $scope.data);
+      auth.$createUserWithEmailAndPassword($scope.data.username, $scope.data.password).then(function(snapshot){
+        console.log('--- addNewUser snapshot --==> ', snapshot);
+        $state.go('login');
+      }, function(error){
+        console.log('--- addNewUser error --==> ', error);
+        $ionicPopup.alert({
+          title: 'Login failed!',
+          template: error.message
+        });
+      });
+  }
 });
